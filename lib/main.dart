0a7e01f2dcb2f870d34a88532603d50945aecf6f
WastefulPines <126472165+WastefulPines@users.noreply.github.com>
2023-10-03 02:18:37 +0800

    New feature: compass direction

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,179 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'dart:async';
+import 'package:vector_math/vector_math_64.dart' as vector;
+import 'package:flutter_compass/flutter_compass.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Location App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late StreamSubscription<Position> _positionStreamSubscription;
+  String _latitude = 'Unknown';
+  String _longitude = 'Unknown';
+  double targetLatitude = 1.453;
+  double targetLongitude = 103.83;
+  String distance = 'Calculating...';
+  String bearing = '0';
+  double heading = 0;
+  double originalDistance = 0;
+  Color textColor = Colors.black; // Initialize text color
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  Future<bool> handlePermission() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+    if (permission == LocationPermission.always ||
+        permission == LocationPermission.whileInUse) {
+      return true;
+    }
+    return false;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _checkPermissionsAndStartLocationUpdates();
+  }
+
+  Future<void> _checkPermissionsAndStartLocationUpdates() async {
+    bool hasPermission = await handlePermission();
+
+    if (hasPermission) {
+      _startLocationUpdates();
+    }
+  }
+
+  @override
+  void dispose() {
+    _stopLocationUpdates();
+    super.dispose();
+  }
+
+  void _calculateDistance(Position currentPosition) {
+    double currentLatitude = currentPosition.latitude;
+    double currentLongitude = currentPosition.longitude;
+    double result = Geolocator.distanceBetween(
+      currentLatitude,
+      currentLongitude,
+      targetLatitude,
+      targetLongitude,
+    );
+    double bearingResult = Geolocator.bearingBetween(
+      currentLatitude,
+      currentLongitude,
+      targetLatitude,
+      targetLongitude,
+    );
+    FlutterCompass.events!.listen((event) {
+      setState(() {
+        heading = bearingResult - event.heading!;
+      });
     });
+    // Check if getting closer or further and update text color accordingly
+    setState(() {
+      _latitude = currentLatitude.toStringAsFixed(6);
+      _longitude = currentLongitude.toStringAsFixed(6);
+      distance = result.toStringAsFixed(2);
+      bearing = bearingResult.toStringAsFixed(2);
+
+      if (result < originalDistance) {
+        textColor = Colors.blue; // Closer, change to blue
+      } else {
+        textColor = Colors.red; // Further, change to red
+      }
+    });
+    originalDistance = result;
+  }
+
+  void _startLocationUpdates() async {
+    const LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.bestForNavigation,
+      distanceFilter: 0,
+    );
+
+    _positionStreamSubscription = Geolocator.getPositionStream(
+      locationSettings: locationSettings,
+    ).listen(
+      (Position position) {
+        _calculateDistance(position);
+      },
+      onError: (error) {
+        setState(() {
+          distance = 'Error: $error';
+        });
+      },
+    );
+  }
+
+  void _stopLocationUpdates() {
+    _positionStreamSubscription.cancel();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Location App'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Text(
+              'Latitude: $_latitude',
+              style: TextStyle(fontSize: 20, color: textColor),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Longitude: $_longitude',
+              style: TextStyle(fontSize: 20, color: textColor),
             ),
+            Text(
+              'Distance: $distance meters',
+              style: TextStyle(fontSize: 20, color: textColor),
+            ),
+            Text(
+              'Bearing: $bearing degrees',
+              style: TextStyle(fontSize: 20, color: textColor),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: Transform.rotate(
+                angle: vector.radians(heading), // Convert degrees to radians
+                child: const Icon(
+                  Icons.arrow_upward,
+                  size: 100,
+                  color: Colors.blue,
+                ),
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
